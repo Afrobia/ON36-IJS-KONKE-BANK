36be18abbc296ea52e4b196dc05144a5ad5362e2
Beatriz Santana <beatrizsantana1993@gmail.com>
2024-07-30 11:02:58 -0300

    criando a base para os controllers

--- src/contas/contas.controller.ts
+++ src/contas/contas.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { ContasService } from './contas.service';
+import { Contas } from './model/contas.model';
 
 @Controller('contas')
-export class ContasController {}
+export class ContasController {
+    constructor(private readonly contaService: ContasService) {}
+
+  @Post()
+  criarConta( @Body('saldo') saldo:number, @Body('tipo') tipo: TipoConta): Contas {
+    return this.contaService.criarConta(saldo,tipo);
+  }
+  
+  @Get()
+  findAll() {
+    return this.contaService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    return this.contaService.findById(+id);
+  }
+
+   @Patch(':id/atualizar-saldo')
+  atualizarSaldo(@Param('id') id: number, @Body("saldo") newSaldo: number) {
+    return this.contaService.atualizarSaldo(+id, newSaldo);
+  }
+
+  @Delete(':id')
+  removerConta(@Param('id') id: number) {
+    return this.contaService.removerConta(+id);
+  } 
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private userService: UserService) {};
+
+    @Post()
+    criarUser(@Body('id') id:string, @Body('nome') nome:string, @Body('tipo') tipo: TipoUser, @Body('telefone') telefone: string, @Body('endereco') endereco: string) {
+      return this.userService.criarUser(id, nome, tipo, endereco, telefone);
+    }
+  
+    @Get()
+    findAll() {
+      return this.userService.findAll();
+    }
+  
+    @Get(':id')
+    findById(@Param('id') id: string) {
+      return this.userService.findById(id);
+    }
+
+    @Delete()
+    removerUser(@Param('id') id:string) {
+        return this.userService.removerUser(id)
+    }
+    
+}
