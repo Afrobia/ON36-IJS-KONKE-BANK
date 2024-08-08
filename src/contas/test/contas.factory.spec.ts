cbd7dfb5785e0aa9d643bde080ed0695f31ca944
Beatriz Santana <beatrizsantana1993@gmail.com>
2024-08-08 10:45:07 -0300

    teste aprovado da contaFactory, conta poupanca

--- src/contas/test/contas.factory.spec.ts
+++ src/contas/test/contas.factory.spec.ts
@@ -2,13 +2,13 @@ import { UserCliente } from "../../cliente/userCliente.model"
 import { TipoConta } from "../enum/tipoConta"
 import { ContasFactory } from "../factories/contas.factory"
 import { ContaCorrente } from "../model/contaCorrente"
+import { ContaPoupanca } from "../model/contaPoupanca"
 
 describe('Contas factory', ()=> {
-
+const cliente = new UserCliente('Beatriz', 'Tão Tão distante', "6565626")  
     //criar conta conrrente
     test("deveria criar uma conta corrente",()=>{
-        const cliente = new UserCliente('Beatriz', 'Tão Tão distante', "6565626")
-
+        
         const contaFactory = new ContasFactory()
         const retornado = contaFactory.criarConta(TipoConta.CORRENTE, cliente)
        
@@ -18,10 +18,22 @@ describe('Contas factory', ()=> {
             expect(retornado.saldo).toBe(0);
 
         }
-    
-    //criar conta poupança
-
-    test 
-        
     })
+    //criar conta poupança
+    test('deveria criar uma conta poupanca', () => {
+        
+        const contaFactory = new ContasFactory()
+        const retornado = contaFactory.criarConta(TipoConta.POUPANCA, cliente)
+       
+        if(retornado instanceof ContaPoupanca){
+            expect(retornado.cliente).toBe(cliente);
+            expect(retornado.taxaRendimento).toBe(0.025);
+            expect(retornado.saldo).toBe(10);
+
+        }
+
+    })
+
+        
+    
 })
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import * as path from 'path';
 import * as fs from 'fs';
 import { User } from './model/user.interface';
-import { UserFactory } from './factories/user.factory';
 
 @Injectable()
 export abstract class UserService {
