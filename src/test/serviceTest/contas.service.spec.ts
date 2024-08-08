9990f0988225a2e777d3e4add858f2870d6a5b5e
Beatriz Santana <beatrizsantana1993@gmail.com>
2024-08-08 16:52:33 -0300

    iniciando os testes no conta Service

--- src/test/serviceTest/contas.service.spec.ts
+++ src/test/serviceTest/contas.service.spec.ts
@@ -2,10 +2,14 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { ContasService } from '../../service/contas.service';
 import { ContasFactory } from '../../factory/contas.factory';
 import { ContasRepository } from '../../repository/contas.repository';
+import { UserCliente } from '../../model/cliente.model';
+import { TipoConta } from '../../enum/conta.enum';
+import { ContaCorrente } from '../..//model/contaFeature/contaCorrente';
 
 describe('ContasService', () => {
   let service: ContasService;
-
+  let cliente = new UserCliente('Fatima', 'Qualquer lugar', 'telefone')
+  
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
         providers: [ContasService, ContasRepository ,ContasFactory],
@@ -17,5 +21,18 @@ describe('ContasService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  test('criarConta, deveria receber uma nova conta da Factory', () =>{
+    
+    const retornado = service.criarConta(TipoConta.CORRENTE, cliente)
+    
+    if(retornado instanceof ContaCorrente){
+      expect(retornado.cliente).toBe(cliente);
+      expect(retornado.chequeEspecial).toBe(150);
+      expect(retornado.saldo).toBe(0);
+      expect(retornado.id).toBeDefined()
+    }
+
+  })
 
 });
